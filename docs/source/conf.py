c5955d7c5f7cfb3e1f68f7d219de4ba94c9532f1
rahimbouchra <rahim.bouchra@gmail.com>
2023-08-23 16:58:33 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'HPC-MARWAN Documentation'
+copyright = '2023, HPC-MARWAN Team'
+author = 'HPC-MARWAN Team'
 
 release = '0.1'
 version = '0.1.0'
